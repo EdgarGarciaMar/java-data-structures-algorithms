298fbc70a7f3d93da8d3958e0abcc359942ca8b8
Edgar García <58317087+EdgarGarciaMar@users.noreply.github.com>
2023-09-06 19:03:37 -0600

    Instructions

--- PalindromeChecker/src/DoublyLinkedList.java
+++ PalindromeChecker/src/DoublyLinkedList.java
@@ -6,6 +6,17 @@
 /**
  *
  * @author edgargarcia
+ * 
+ DLL: Palindrome Checker ( ** Interview Question)
+Write a method to determine whether a given doubly linked list reads the same forwards and backwards.
+
+For example, if the list contains the values [1, 2, 3, 2, 1], then the method should return true, since the list is a palindrome.
+
+If the list contains the values [1, 2, 3, 4, 5], then the method should return false, since the list is not a palindrome.
+
+Method name: isPalindrome 
+
+Return Type: boolean
  */
 public class DoublyLinkedList {
 
