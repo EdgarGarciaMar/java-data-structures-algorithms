6cbacf45507b18d0f743e5af6dca52a37f8f069d
Edgar García <58317087+EdgarGarciaMar@users.noreply.github.com>
2023-09-06 19:00:40 -0600

    DLL: Palindrome Checker

---
+++ PalindromeChecker/src/DoublyLinkedList.java
@@ -0,0 +1,108 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author edgargarcia
+ */
+public class DoublyLinkedList {
+
+    private Node head;
+    private Node tail;
+    private int length;
+
+    class Node {
+
+        int value;
+        Node next;
+        Node prev;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public DoublyLinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public Node getTail() {
+        return tail;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void printAll() {
+        if (length == 0) {
+            System.out.println("Head: null");
+            System.out.println("Tail: null");
+        } else {
+            System.out.println("Head: " + head.value);
+            System.out.println("Tail: " + tail.value);
+        }
+        System.out.println("Length:" + length);
+        System.out.println("\nDoubly Linked List:");
+        if (length == 0) {
+            System.out.println("empty");
+        } else {
+            printList();
+        }
+    }
+
+    public void makeEmpty() {
+        head = null;
+        tail = null;
+        length = 0;
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        length++;
+    }
+
+    boolean isPalindrome() {
+        if (head == null && tail == null) {
+            return true;
+        }
+        Node temp1 = head;
+        Node temp2 = tail;
+
+        for (int i = 0; i <= (length / 2); i++) {
+            if (temp1.value != temp2.value) {
+                return false;
+            }
+            temp1 = temp1.next;
+            temp2 = temp2.prev;
+        }
+        return true;
+
+    }
+
+}
