d1ff87dd931b205d244b34a36d19eeeb79383b31
Edgar García <58317087+EdgarGarciaMar@users.noreply.github.com>
2023-09-10 18:28:08 -0600

    Stack

---
+++ Stack/src/Main.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author edgargarcia
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        Stack myStack = new Stack(2);
+        myStack.push(1);
+
+        // (2) Items - Returns 1 Node
+        System.out.println(myStack.pop().value);
+        // (1) Item - Returns 2 Node
+        System.out.println(myStack.pop().value);
+        // (0) Items - Returns null
+        System.out.println(myStack.pop());
+
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            1
+            2
+            null
+
+        */
+
+    }
+
+}
