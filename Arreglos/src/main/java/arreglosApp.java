3d2220d583499916a27c4307a571aef241604cbf
Edgar García <58317087+EdgarGarciaMar@users.noreply.github.com>
2025-04-28 21:46:00 -0600

    implementacion heap

--- Arreglos/src/main/java/arreglosApp.java
+++ Arreglos/src/main/java/arreglosApp.java
@@ -9,26 +9,40 @@ public class arreglosApp {
         int tam = scanner.nextInt();
         arrayClase a = new arrayClase(tam);
         a.guardarDatos();
-        System.out.println("Seleccione una de las siguientes opciones: ");
-        System.out.println("1: Imprimir datos\n" +
-                "2: Busqueda linear\n" +
-                "3: Busqueda binaria\n");
-        int op =scanner.nextInt();
-        switch (op){
-            case 1 :
-                a.imprimirDatos();
-            case 2 :
-                System.out.println("Ingresa el número a buscar: ");
-                int val = scanner.nextInt();
-                int res = a.busquedaLinear(a.getArr(),tam,val);
-                if(res == -1){
-                    System.out.println("No se encontro el "+val+" en el arreglo");
-                }else{
-                    res+=1;
-                    System.out.println("El "+val+" Se encuentra en la pos :"+res);
-                }
+        while(true) {
+            System.out.println("+++++++++++++++++++++++++++++++++++++++++++");
+            System.out.println("Seleccione una de las siguientes opciones: ");
+            System.out.println("1: Imprimir datos\n" +
+                    "2: Busqueda linear\n" +
+                    "3: Heap sort\n" +
+                    "4: Salir\n");
+            System.out.println("Introduce tu selección:");
+            int op =scanner.nextInt();
+            switch (op) {
+                case 1:
+                    a.imprimirDatos();
+                    break;
+                case 2:
+                    System.out.println("Ingresa el número a buscar: ");
+                    int val = scanner.nextInt();
+                    int res = a.busquedaLinear(a.getArr(), tam, val);
+                    if (res == -1) {
+                        System.out.println("No se encontro el " + val + " en el arreglo");
+                    } else {
+                        res += 1;
+                        System.out.println("El " + val + " Se encuentra en la pos :" + res);
+                    }
+                    break;
+                case 3:
+                    a.heapSort(a.getArr());
+                    a.imprimirDatos();
+                    break;
+                case 4:
+                    return;
+                default:
+                    System.out.println("tralalero tralala");
+            }
         }
-
 
 
     }
